13c123f17f4bbe5b92f533869af1c4358854daa5
100yo <stoyan.vellev@sap.com>
2018-10-12 11:38:56 +0300

    Switch spaces to tabs

--- 01-intro-to-java/lab/02-anagrams/SampleAnagramTest.java
+++ 01-intro-to-java/lab/02-anagrams/SampleAnagramTest.java
@@ -1,36 +1,35 @@
-import org.junit.Before;
-import org.junit.Test;
-
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+import org.junit.Before;
+import org.junit.Test;
+
 public class SampleAnagramTest {
 
-    private Anagram anagram;
+	private Anagram anagram;
 
-    @Before
-    public void setUp() {
-        anagram = new Anagram();
-    }
+	@Before
+	public void setUp() {
+		anagram = new Anagram();
+	}
 
-    @Test
-    public void testAnagrams_PlainAnagrams() {
-        assertTrue(anagram.isAnagram("night thing"));
-    }
+	@Test
+	public void testAnagrams_PlainAnagrams() {
+		assertTrue(anagram.isAnagram("night thing"));
+	}
 
-    @Test
-    public void testAnagrams_MissingLetter() {
-        assertFalse(anagram.isAnagram("tired dire"));
-    }
+	@Test
+	public void testAnagrams_MissingLetter() {
+		assertFalse(anagram.isAnagram("tired dire"));
+	}
 
-    @Test
-    public void testAnagrams_IgnoreCaseAndSpecialChars() {
-        assertTrue(anagram.isAnagram("eaT ate$"));
-    }
+	@Test
+	public void testAnagrams_IgnoreCaseAndSpecialChars() {
+		assertTrue(anagram.isAnagram("eaT ate$"));
+	}
 
-    @Test
-    public void testAnagrams_SameLettersDifferentCount() {
-        assertFalse(anagram.isAnagram("beer beere"));
-    }
-
+	@Test
+	public void testAnagrams_SameLettersDifferentCount() {
+		assertFalse(anagram.isAnagram("beer beere"));
+	}
 }
