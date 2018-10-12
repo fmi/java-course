8b2a3ed5cd3c8d7b71f53c0d717497063af66ac5
100yo <stoyan.vellev@sap.com>
2018-10-12 11:33:46 +0300

    Add anagrams assignment

---
+++ 01-intro-to-java/lab/02-anagrams/SampleAnagramTest.java
@@ -0,0 +1,36 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class SampleAnagramTest {
+
+    private Anagram anagram;
+
+    @Before
+    public void setUp() {
+        anagram = new Anagram();
+    }
+
+    @Test
+    public void testAnagrams_PlainAnagrams() {
+        assertTrue(anagram.isAnagram("night thing"));
+    }
+
+    @Test
+    public void testAnagrams_MissingLetter() {
+        assertFalse(anagram.isAnagram("tired dire"));
+    }
+
+    @Test
+    public void testAnagrams_IgnoreCaseAndSpecialChars() {
+        assertTrue(anagram.isAnagram("eaT ate$"));
+    }
+
+    @Test
+    public void testAnagrams_SameLettersDifferentCount() {
+        assertFalse(anagram.isAnagram("beer beere"));
+    }
+
+}
