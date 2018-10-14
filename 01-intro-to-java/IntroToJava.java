79c0ba35b6738b95a53a6d20652bf05b286c8589
ialidzhikov <i.alidjikov@gmail.com>
2018-10-14 22:03:27 +0300

    Fix renaming

--- 01-intro-to-java/IntroToJava.java
+++ 01-intro-to-java/IntroToJava.java
@@ -3,95 +3,123 @@ import java.util.Collections;
 
 public class IntroToJava {
 
-	public static void workWithStrings() {
-
+	public static void exploreString() {
 		// 1. string literals and the string pool
-		String s1 = "FMI"; // goes to string pool
-		String s2 = "FMI"; // "FMI" is present in the String pool -> s2 will refer to the same object
+		String literalOne = "FMI"; // goes to string pool
+		String literalTwo = "FMI"; // "FMI" is present in the String pool -> literalTwo will refer to the same object
 
-		System.out.println(s1 == s2); // true
+		System.out.println(literalOne == literalTwo); // true
 
-		String s3 = new String("FMI"); // new String object in the heap
+		String newString = new String("FMI"); // new String object in the heap
 
-		System.out.println(s1 == s3); // false, s1 refers to an object in the string pools, s2 refers to an object in the heap
-		System.out.println(s1.equals(s3)); // true: string content is the same
+		System.out.println(literalOne == newString); // false, literalOne refers to an object in the string pools, newString refers to an object in the heap
+		System.out.println(literalOne.equals(newString)); // true: string content is the same
 
-		String s4 = s3.intern(); //  String "interning" will add the string in the pool and return a reference to it
-		System.out.println(s1 == s3); // false, s3 is not reassigned
-		System.out.println(s1 == s4); // true
+		String intern = newString.intern(); //  String "interning" will add the string in the pool and return a reference to it
+		System.out.println(literalOne == newString); // false, newString is not reassigned
+		System.out.println(literalOne == intern); // true
 
 		// 2. string concatenation and splitting
 		int year = 2018;
 		String message = "Current year is ";
 		message += 2018 + "."; // String are immutable so this will create a new String object and some garbage
 
-		String[] words = message.split(" ");
+		String[] words = message.split(" "); // [Current, year, is, 2018.]
 
 		// 3. string iteration and conversion String -> char array
-		String s5 = "Example";
-		char c = s5.charAt(1); // 'x'. Note that Strings are not char arrays, i.e. you cannot do e.g. s5[1];
-		char[] carr = s5.toCharArray(); // convert a String to char array
+		String example = "Example";
+		char secondChar = example.charAt(1); // 'x'. Note that Strings are not char arrays, i.e. you cannot do e.g. example[1];
+		char[] chars = example.toCharArray(); // convert a String to char array
 
-		for (int i = 0; i < carr.length; i++) { // iterate the char array with standard for-loop
-			System.out.println(carr[i]);
+		for (int i = 0; i < chars.length; i++) { // iterate the char array with standard for-loop
+			System.out.println(chars[i]);
 		}
 
-		for (char ch : carr) { // the same as above but with for-each loop -> recommended because it's shorter
-			System.out.println(ch);
+		for (char current : chars) { // the same as above but with for-each loop -> recommended because it's shorter
+			System.out.println(current);
 		}
 
 		// 4. conversion char array -> String
-		String backToStr1 = String.valueOf(carr); // note that carr.toString() does not work as you may expect
-		String backToStr2 = Arrays.toString(carr); // does the same
+		String backToStr1 = String.valueOf(chars); // note that carr.toString() does not work as you may expect
+		String backToStr2 = Arrays.toString(chars); // does the same
 
 		// 5. String manipulation
 		String lower = "WHatEveR".toLowerCase(); // "whatever"
 		String upper = "WHatEveR".toUpperCase(); // "WHATEVER"
 		String reverse = new StringBuilder(lower).reverse().toString(); // "revetahw"
+		
+		String replaced = "cut".replace('c', 'p'); // "put"
+		String substring = "Anaconda".substring(2, 6); // "acon"
+		boolean containsCo = substring.contains("co"); // true
+		boolean startsWithB = substring.startsWith("b"); // false
+		boolean endsWithOn = substring.endsWith("on"); // true
+		int indexOfN = "Coconut".indexOf("n"); // 4
+		int length = "Coconut".length(); // 7 (note that the length is a method, not a field)
+	}
 
-		String repl = "cut".replace('c', 'p'); // "put"
-		String sub = "Anaconda".substring(2, 6); // "acon"
-		boolean con = sub.contains("co"); // true
-		boolean star = sub.startsWith("b"); // false
-		boolean endsWith = sub.endsWith("on"); // true
-		int ind = "Coconut".indexOf("n"); // 4
-		int len = "Coconut".length(); // 7
+	public static void exploreStringBuilder() {
+		// We use StringBuilder when we expect the string to be manipulated
+		// e.g. in loops:
+		StringBuilder oddNumbers = new StringBuilder();
+		for (int i = 0; i < 20; i++) {
+			if (i % 2 != 0) {
+				oddNumbers.append(i).append(" ");
+			}
+		}
+		System.out.println(oddNumbers); // "0 2 4 6 8 10 12 14 16 18 "
 
-		// 6. Arrays
+		// or in something like this:
+		StringBuilder word = new StringBuilder("racecar");
+		boolean isPalindrome = word.equals(word.reverse()); // true
 
-		int[] iarr; // just declaration:the array is not created, no memory is allocated
-		iarr = new int[100]; // initialization
-		String[] sarr = {"apple", "banana", "cherry"}; // string arrays with 3 elements is declared and initialized
-		char[] ca = new char[]{'a', 'b'}; // such initialization is also valid
-		int l = sarr.length; // 3
+		// As the following concatenation is in the same statement,
+		// the compiler will optimize this and use StringBuilder instead
+		String feelings = "I " + "<3 " + "Java";
+	}
 
-		long[][] l_matrix = new long[10][10]; // 10x10 array initialized
-		double[][] d_matrix = new double[100][]; // array with 100 rows initialized, but all rows still uninitialized
+	public static void exploreArrays() {
+		int[] array; // just declaration - the array is not created, no memory is allocated
+		array = new int[5]; // initialization - now we have [0, 0, 0, 0, 0]
+
+		String[] stringArray = { "apple", "banana", "cherry" }; // string array with 3 elements is declared and initialized
+		char[] charArray = new char[] { 'a', 'b' }; // such initialization is also valid
+		int length = stringArray.length; // 3 (note that the length is a field, not a method)
+
+		long[][] longMatrix = new long[10][10]; // 10x10 array initialized
+		double[][] doubleMatrix = new double[100][]; // array with 100 rows initialized, but all rows still uninitialized
 
 		for (int i = 0; i < 10; i++) {
-			d_matrix[i] = new double[10]; // array now initialized. Note that rows need not be of the same length
+			doubleMatrix[i] = new double[10]; // array now initialized. Note that rows need not be of the same length
 		}
 
-		int[] ia = {1, 6, 7, 3};
-		Arrays.sort(ia); // {1, 3, 6, 7}
-		String[] sa = {"cat", "dog", "bird"};
-		Arrays.sort(sa, Collections.reverseOrder()); // {"dog", "cat", "bird"}
+		int[] intArray = { 1, 6, 7, 3 };
+		Arrays.sort(intArray); // { 1, 3, 6, 7 }
+		String[] animals = { "cat", "dog", "bird" };
+		Arrays.sort(animals, Collections.reverseOrder()); // { "dog", "cat", "bird" }
 
-		for (String s : sa) {
-			System.out.println(s);
+		for (String animal : animals) {
+			System.out.println(animal);
 		}
 
-		int index = Arrays.binarySearch(ia, 7); // 3
+		int index = Arrays.binarySearch(intArray, 7); // 3
 
-		int[] copy = Arrays.copyOf(ia, ia.length);
-		boolean areEqual = Arrays.equals(ia, copy); // true
+		int[] copy = Arrays.copyOf(intArray, intArray.length);
+		boolean areEqual = Arrays.equals(intArray, copy); // true
 
 		System.out.println(Arrays.toString(copy));
 
+		// As we know, arrays are objects and need to be compared with "equals()".
+		// However, two (and more) dimensional arrays are arrays of objects
+		// and Java has a special method for comparing them:
+		char[][] currentBoard = { {'X','O','X'}, {'X','O','O'}, {'X','X','O'} };
+		char[][] winBoard = { {'X','O','X'}, {'X','O','O'}, {'X','X','O'} };
+		boolean isWinning = Arrays.deepEquals(currentBoard, winBoard); // true
 	}
 
 	public static void main(String... args) {
-		workWithStrings();
+		exploreString();
+		exploreStringBuilder();
+		exploreArrays();
 	}
 
 }
