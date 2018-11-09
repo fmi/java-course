581e75abdb31c3d4e80026b1a214e522fd502ed6
Yordanova <gabriela.yordanova@sap.com>
2018-11-09 16:26:02 +0200

    fixed conflicts in lab1

--- 01-intro-to-java/lab/03-forest-terrain/SampleForestTrackerTest.java
+++ 01-intro-to-java/lab/03-forest-terrain/SampleForestTrackerTest.java
@@ -1,42 +1,10 @@
-<<<<<<< HEAD
-import java.util.Arrays;
-
-import org.junit.Assert;
-=======
 import static org.junit.Assert.assertArrayEquals;
 
 import org.junit.Before;
->>>>>>> upstream/master
 import org.junit.Test;
 
-public class SampleForestTrackerTest {
+public class ForestTrackerTest {
 
-<<<<<<< HEAD
-	private static ForestTracker tracker = new ForestTracker();
-	
-	@Test
-	public void testIfForestStaysTheSameForLessThan10Years() {
-		char[][] forest = {{'S', '1', 'S', '1', 'R'},
-						  {'S', '2', '4', '4', '4'},
-						  {'1', '3', 'S', '3', '4'}};
-
-		Assert.assertTrue(Arrays.deepEquals(forest, tracker.trackForestTerrain(forest, -10)));
-		Assert.assertTrue(Arrays.deepEquals(forest, tracker.trackForestTerrain(forest, 5)));
-	}
-	
-	@Test
-	public void testIfDensity4BecomesDensity3AndOtherDensitiesChangeCorrectly() {
-		char[][] originalForest = {{'R', 'R', 'S', '1', '4'},
-								   {'S', '2', '4', '4', '2'},
-								   {'1', '3', 'S', '3', '4'}};
-		
-		char[][] expectedForest = {{'R', 'R', 'S', '2', '4'},
-						  		   {'S', '3', '4', '3', '3'},
-						  		   {'2', '4', 'S', '4', '4'}};
-		
-		Assert.assertTrue(Arrays.deepEquals(expectedForest, tracker.trackForestTerrain(originalForest, 10)));
-		Assert.assertTrue(Arrays.deepEquals(expectedForest, tracker.trackForestTerrain(originalForest, 15)));
-=======
 	private ForestTracker tracker;
 
 	@Before
@@ -66,6 +34,94 @@ public class SampleForestTrackerTest {
 
 		assertArrayEquals(expected, tracker.trackForestTerrain(forest, 10));
 		assertArrayEquals(expected, tracker.trackForestTerrain(forest, 15));
->>>>>>> upstream/master
+	}
+
+	@Test
+	public void testTrack_EachDensityChangesCorrectly() {
+		char[][] ones = { {'1', '1', '1'},
+						  {'1', '1', '1'},
+						  {'1', '1', '1'}, };
+
+		char[][] twos = { {'2', '2', '2'},
+						  {'2', '2', '2'},
+						  {'2', '2', '2'}, };
+
+		char[][] threes = { {'3', '3', '3'},
+							{'3', '3', '3'},
+							{'3', '3', '3'}, };
+
+		char[][] fours = { {'4', '4', '4'},
+						   {'4', '4', '4'},
+						   {'4', '4', '4'}, };
+
+		assertArrayEquals(ones, tracker.trackForestTerrain(ones, -10));
+		assertArrayEquals(ones, tracker.trackForestTerrain(ones, 5));
+		assertArrayEquals(twos, tracker.trackForestTerrain(ones, 10));
+
+		assertArrayEquals(twos, tracker.trackForestTerrain(twos, -10));
+		assertArrayEquals(twos, tracker.trackForestTerrain(twos, 5));
+		assertArrayEquals(threes, tracker.trackForestTerrain(twos, 10));
+
+		assertArrayEquals(threes, tracker.trackForestTerrain(threes, -10));
+		assertArrayEquals(threes, tracker.trackForestTerrain(threes, 5));
+		assertArrayEquals(fours, tracker.trackForestTerrain(threes, 10));
+
+		assertArrayEquals(fours, tracker.trackForestTerrain(fours, -10));
+		assertArrayEquals(fours, tracker.trackForestTerrain(fours, 5));
+		assertArrayEquals(threes, tracker.trackForestTerrain(fours, 10));
+		assertArrayEquals(fours, tracker.trackForestTerrain(fours, 40));
+	}
+
+	@Test
+	public void testTrack_StaticFieldsStayTheSame() {
+		char[][] river = { {'R', 'R', 'R'},
+						   {'R', 'R', 'R'},
+						   {'R', 'R', 'R'}, };
+
+		char[][] rock = { {'S', 'S', 'S'},
+						  {'S', 'S', 'S'},
+						  {'S', 'S', 'S'}, };
+
+		assertArrayEquals(river, tracker.trackForestTerrain(river, 10));
+		assertArrayEquals(rock, tracker.trackForestTerrain(rock, 10));
+	}
+
+	@Test
+	public void testTrack_Density4StaysTheSame() {
+		char[][] forest = { {'R', 'R', 'S'},
+							{'4', '4', 'R'},
+							{'S', '4', 'S'}, };
+
+		assertArrayEquals(forest, tracker.trackForestTerrain(forest, -5));
+		assertArrayEquals(forest, tracker.trackForestTerrain(forest, 10));
+		assertArrayEquals(forest, tracker.trackForestTerrain(forest, 20));
+	}
+
+	@Test
+	public void testTrack_MixedDensityWithout4ChangesCorrectly() {
+		char[][] forest = { {'R', '2', '1'},
+							{'4', '4', '3'},
+							{'S', '2', '3'}, };
+
+		char[][] expected = { {'R', '3', '2'},
+							  {'4', '4', '4'},
+							  {'S', '3', '4'}, };
+
+		assertArrayEquals(expected, tracker.trackForestTerrain(forest, 10));
+		assertArrayEquals(expected, tracker.trackForestTerrain(forest, 15));
+	}
+
+	@Test
+	public void testTrack_MultupleNeighbourDensities4ChangeCorrectly() {
+		char[][] forest = { {'R', '2', '1'},
+							{'4', '4', '3'},
+							{'4', '4', '1'}, };
+
+		char[][] expected = { {'R', '3', '2'},
+							  {'3', '3', '4'},
+							  {'3', '3', '2'}, };
+
+		assertArrayEquals(expected, tracker.trackForestTerrain(forest, 10));
+		assertArrayEquals(expected, tracker.trackForestTerrain(forest, 15));
 	}
 }
