9bea47e6c097c75e23a526e746aa624644de155d
Kristian <kristianzh@abv.bg>
2018-11-27 18:50:44 +0200

    Add second homework (#34)

---
+++ homeworks/02-movie-review-sentiment-analyzer/SampleMovieReviewSentimentAnalyzerTest.java
@@ -0,0 +1,47 @@
+package bg.sofia.uni.fmi.mjt.sentiment;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class MovieReviewSentimentAnalyzerTest {
+
+	private MovieReviewSentimentAnalyzer analyzer;
+
+	private InputStream reviewsStream;
+	private InputStream stopwordsStream;
+	private OutputStream resultStream;
+
+	@Before
+	public void init() throws FileNotFoundException {
+		stopwordsStream = new FileInputStream("resources/stopwords.txt");
+		reviewsStream = new FileInputStream("resources/reviews.txt");
+		resultStream = new FileOutputStream("resources/reviews.txt", true);
+		analyzer = new MovieReviewSentimentAnalyzer(stopwordsStream, reviewsStream, resultStream);
+	}
+
+	@Test
+	public void testIsStopWordNegativeFromDictionary() {
+		String assertMessage = "A word should not be incorrectly identified as a stopword, if it is not part of the stopwords list";
+		assertFalse(assertMessage, analyzer.isStopWord("effects"));
+	}
+
+	@Test
+	public void testIsStopWordNegativeNotFromDictionary() {
+		String assertMessage = "A word should not be incorrectly identified as a stopword, if it is not part of the stopwords list";
+		assertFalse(assertMessage, analyzer.isStopWord("stoyo"));
+	}
+
+	@Test
+	public void testIsStopWordPositive() {
+		assertTrue("Stop word not counted as stop word", analyzer.isStopWord("a"));
+	}
+}
