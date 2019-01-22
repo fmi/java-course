eda42d3379f9d75ef0d0cca0207353bc0879551e
100yo <stoyan.vellev@sap.com>
2019-01-22 18:53:42 +0200

    Add full reference solution to 09-networking Lab assignment

--- 09-network/lab/solution/test/bg/sofia/uni/fmi/java/network/server/nio/CommandExecutionServerTest.java
+++ 09-network/lab/solution/test/bg/sofia/uni/fmi/java/network/server/nio/CommandExecutionServerTest.java
@@ -16,84 +16,76 @@ import org.junit.Test;
 
 public class CommandExecutionServerTest {
 
-	private static Thread serverStarterThread;
-	private static CommandExecutionServer commandExServer;
-	
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
-		serverStarterThread = new Thread() {
-			
-			public void run() { 
-				try (CommandExecutionServer ces = new CommandExecutionServer(CommandExecutionServer.SERVER_PORT);) {
-					commandExServer = ces;
-					commandExServer.start();
-				} catch (Exception e) {
-					System.out.println("An error has occured");
-					e.printStackTrace();
-				}
-			}
-		}; 
-		serverStarterThread.start();
+    private static Thread serverStarterThread;
+    private static CommandExecutionServer commandExServer;
 
-		while (true) {
-			try (Socket socket = new Socket("localhost", CommandExecutionServer.SERVER_PORT)) {
-				break; // client connection accepted --> server has started
-			} finally {
-				Thread.sleep(500);
-			}
-		}
-	}
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception {
+        serverStarterThread = new Thread() {
 
-	@AfterClass
-	public static void tearDownAfterClass() throws Exception {
-		commandExServer.stop();
-		// Wake up the server so that it can exit
-		serverStarterThread.interrupt();
-	}
+            public void run() {
+                try (CommandExecutionServer ces = new CommandExecutionServer(CommandExecutionServer.SERVER_PORT);) {
+                    commandExServer = ces;
+                    commandExServer.start();
+                } catch (Exception e) {
+                    System.out.println("An error has occured");
+                    e.printStackTrace();
+                }
+            }
+        };
+        serverStarterThread.start();
+    }
 
-	@Test
-	public void test_01_EchoCommandWithCorrectData() {
-		assertEquals("what", sendRecvMsg("echo:what"));
-	}
-	
-	@Test
-	public void test_02_EchoCommandWithIncorrectData() {
-		assertEquals("Missing Argument", sendRecvMsg("echo"));
-	}
-	
-	@Test
-	public void test_03_UnknownCommand() {
-		assertEquals("Unknown command", sendRecvMsg("test"));
-	}
-	
-	@Test
-	public void test_04_GetHostnameCommand() throws UnknownHostException {
-		assertEquals(InetAddress.getLocalHost().getHostName(), sendRecvMsg("gethostname"));
-	}
-	
-	@Test
-	public void test_05_CommandWithAdditionalDelimiter() {
-		assertEquals("Incorrect command syntax", sendRecvMsg("echo:what:what"));
-	}
+    @AfterClass
+    public static void tearDownAfterClass() throws Exception {
+        commandExServer.stop();
+        // Wake up the server so that it can exit
+        serverStarterThread.interrupt();
+    }
 
-	private String sendRecvMsg(String msg) {
-		String response = "fail";
-		try (Socket socket = new Socket("localhost", CommandExecutionServer.SERVER_PORT);
-				BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-				PrintWriter out = new PrintWriter(socket.getOutputStream())) {
-			System.out.println("Client " + socket + " connected to server");
+    @Test
+    public void testEchoCommandWithCorrectData() {
+        assertEquals("what", sendRecvMsg("echo:what"));
+    }
 
-			out.println(msg);
-			out.flush();
+    @Test
+    public void testEchoCommandWithIncorrectData() {
+        assertEquals("Missing Argument", sendRecvMsg("echo"));
+    }
 
-			// Read the response from the server
-			response = in.readLine();
-		} catch (IOException e) {
-			System.out.println("An error has occured " + e.getMessage());
-			e.printStackTrace();
-		}
+    @Test
+    public void testUnknownCommand() {
+        assertEquals("Unknown command", sendRecvMsg("test"));
+    }
 
-		return response;
-	}
+    @Test
+    public void testGetHostnameCommand() throws UnknownHostException {
+        assertEquals(InetAddress.getLocalHost().getHostName(), sendRecvMsg("gethostname"));
+    }
+
+    @Test
+    public void testCommandWithAdditionalDelimiter() {
+        assertEquals("Incorrect command syntax", sendRecvMsg("echo:what:what"));
+    }
+
+    private String sendRecvMsg(String msg) {
+        String response = "fail";
+        try (Socket socket = new Socket("localhost", CommandExecutionServer.SERVER_PORT);
+                BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                PrintWriter out = new PrintWriter(socket.getOutputStream())) {
+            System.out.println("Client " + socket + " connected to server");
+
+            out.println(msg);
+            out.flush();
+
+            // Read the response from the server
+            response = in.readLine();
+        } catch (IOException e) {
+            System.out.println("An error has occured " + e.getMessage());
+            e.printStackTrace();
+        }
+
+        return response;
+    }
 
 }
