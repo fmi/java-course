67c72254ea47b0cac4b339e8496243ff575c09ef
tuechki <dtuechki@gmail.com>
2019-10-12 11:35:31 +0300

    Change expected and actual values according to assertEquals specification. (#54)

--- 01-intro-to-java/lab/02-fraction-simplifier/SampleFractionSimplifierTest.java
+++ 01-intro-to-java/lab/02-fraction-simplifier/SampleFractionSimplifierTest.java
@@ -14,22 +14,22 @@ public class SampleFractionSimplifierTest {
 
     @Test
     public void testFraction_ProperFraction() {
-        assertEquals(fractionSimplifier.simplify("4/6"), "2/3");
+        assertEquals("2/3", fractionSimplifier.simplify("4/6"));
     }
 
     @Test
     public void testFraction_ProperSimplifiedFraction() {
-        assertEquals(fractionSimplifier.simplify("10/11"), "10/11");
+        assertEquals("10/11", fractionSimplifier.simplify("10/11"));
     }
 
     @Test
     public void testFraction_ProperFractionManyCommonDivisors() {
-        assertEquals(fractionSimplifier.simplify("100/400"), "1/4");
+        assertEquals("1/4", fractionSimplifier.simplify("100/400"));
     }
 
     @Test
     public void testFraction_ImproperFractionToInteger() {
-        assertEquals(fractionSimplifier.simplify("8/4"), "2");
+        assertEquals("2", fractionSimplifier.simplify("8/4"));
     }
 
 }
