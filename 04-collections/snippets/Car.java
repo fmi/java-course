6df38824f050ce950792382263abb268a0f89c7b
100yo <stoyan.vellev@sap.com>
2019-11-02 00:48:02 +0200

    Fix typos and formatting

--- 04-collections/snippets/Car.java
+++ 04-collections/snippets/Car.java
@@ -1,39 +1,39 @@
 public class Car implements Comparable<Car> {
 
-	private int topSpeed;
-	private String colour;
-	private String brand;
+    private int topSpeed;
+    private String color;
+    private String brand;
 
-	public int getTopSpeed() {
-		return topSpeed;
-	}
+    public int getTopSpeed() {
+        return topSpeed;
+    }
 
-	public String getColour() {
-		return getColour();
-	}
+    public String getColor() {
+        return getColor();
+    }
 
-	public String getBrand() {
-		return brand;
-	}
+    public String getBrand() {
+        return brand;
+    }
 
-	public Car(int topSpeed, String colour, String model) {
-		this.topSpeed = topSpeed;
-		this.colour = colour;
-		this.brand = model;
-	}
+    public Car(int topSpeed, String color, String model) {
+        this.topSpeed = topSpeed;
+        this.color = color;
+        this.brand = model;
+    }
 
-	@Override
-	public int compareTo(Car other) {
-		//let's sort the cars based on an topSpeed in ascending order
-		//returns a negative integer, zero, or a positive integer as this car topSpeed
-		//is less than, equal to, or greater than the specified object.
-		//return (other.topSpeed - this.topSpeed);
-		return (this.topSpeed - other.topSpeed);
-	}
+    @Override
+    public int compareTo(Car other) {
+        // Let's sort the cars by top speed in ascending order.
+        // The method returns a negative integer, zero, or a positive integer when this car's top speed
+        // is less than, equal to, or greater than the top speed of the specified object.
+        // return (other.topSpeed - this.topSpeed);
+        return (this.topSpeed - other.topSpeed);
+    }
 
-	@Override
-	public String toString() {
-		return "[topSpeed=" + this.topSpeed + ", colour=" + this.colour + ", model=" + this.brand + "]";
-	}
+    @Override
+    public String toString() {
+        return "[topSpeed=" + this.topSpeed + ", color=" + this.color + ", model=" + this.brand + "]";
+    }
 
 }
