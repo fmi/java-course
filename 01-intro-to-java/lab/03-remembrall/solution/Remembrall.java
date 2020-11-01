2d780816eb9922b0975b43aa3cc9d98f8e80a343
100yo <stoyan.vellev@sap.com>
2020-11-01 18:24:36 +0200

    Add improved solution to Lab 01.3 assignment

--- 01-intro-to-java/lab/03-remembrall/solution/Remembrall.java
+++ 01-intro-to-java/lab/03-remembrall/solution/Remembrall.java
@@ -1,23 +1,28 @@
-import java.util.Arrays;
-
 public class Remembrall {
 
+    // O(N) algorithm suggested by Vladimir Karachomakov
+
     public static boolean isPhoneNumberForgettable(String phoneNumber) {
+
         if (phoneNumber == null || phoneNumber.isBlank()) {
             return false;
         }
 
-        phoneNumber = phoneNumber.replace("-", "").replace(" ", "");
+        char[] arr = phoneNumber.toCharArray();
+        boolean[] visited = new boolean[10];
 
-        char[] chars = phoneNumber.toCharArray();
-        Arrays.sort(chars);
-        for (int i = chars.length - 1; i >= 1; i--) {
-            if (Character.isLetter(chars[i])) {
-                return true;
+        for (char c : arr) {
+            if (c == '-' || c == ' ') {
+                continue;
             }
-
-            if (chars[i] == chars[i - 1]) {
-                return false;
+            int digit = c - '0';
+            if (digit >= 0 && digit < 10) {
+                if (visited[digit]) {
+                    return false;
+                }
+                visited[digit] = true;
+            } else {
+                return true;
             }
         }
 
