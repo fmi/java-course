552daa6b886f1f6b099ef7127e9b750b12e96a7b
desislavaa <dess.95@abv.bg>
2020-11-26 13:21:30 +0200

    Add MJTExpressWarehouse solution

---
+++ 05-generics/lab/solution/src/bg/sofia/uni/fmi/mjt/warehouse/exceptions/CapacityExceededException.java
@@ -0,0 +1,4 @@
+package bg.sofia.uni.fmi.mjt.warehouse.exceptions;
+
+public class CapacityExceededException extends Exception {
+}
---
+++ 05-generics/lab/solution/src/bg/sofia/uni/fmi/mjt/warehouse/exceptions/ParcelNotFoundException.java
@@ -0,0 +1,4 @@
+package bg.sofia.uni.fmi.mjt.warehouse.exceptions;
+
+public class ParcelNotFoundException extends Exception {
+}
