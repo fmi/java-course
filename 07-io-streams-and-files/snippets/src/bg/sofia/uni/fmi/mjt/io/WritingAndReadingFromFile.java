521fdc90d5b6f8e0a66b3dcf4a935ad4bad18db7
Ivan Angelov <inangelov@abv.bg>
2020-12-12 12:38:08 +0200

    Update WritingAndReadingFromFile.java

--- 07-io-streams-and-files/snippets/src/bg/sofia/uni/fmi/mjt/io/WritingAndReadingFromFile.java
+++ 07-io-streams-and-files/snippets/src/bg/sofia/uni/fmi/mjt/io/WritingAndReadingFromFile.java
@@ -7,38 +7,33 @@ import java.nio.file.Path;
 
 public class WritingAndReadingFromFile {
 
-    public static void main(String... args) {
+	public static void main(String... args) {
+		Path filePath = Path.of("writingAndReadingFromFile.txt");
+		String text = "Write this string to my file" + System.lineSeparator();
 
-        Path file = Path.of("writingAndReadingFromFile.txt");
-        String text = "Write this string to my file" + System.lineSeparator();
+		writeToFile(filePath, text);
+		readFromFile(filePath);
+	}
 
-        writeToFile(file, text);
-        readFromFile(file);
-    }
+	private static void writeToFile(Path filePath, String text) {
+		try (var bufferedWriter = Files.newBufferedWriter(filePath)) {
+			bufferedWriter.write(text);
+			bufferedWriter.flush();
+		} catch (IOException e) {
+			throw new IllegalStateException("A problem occured while writing to a file", e);
+		}
+	}
 
-    private static void writeToFile(Path file, String text) {
+	private static void readFromFile(Path filePath) {
+		try (BufferedReader bufferedReader = Files.newBufferedReader(filePath)) {
+			String line;
 
-        try (var fw = Files.newBufferedWriter(file)) {
-            fw.write(text);
-            fw.flush();
-
-        } catch (IOException e) {
-            throw new RuntimeException();
-        }
-
-    }
-
-    private static void readFromFile(Path file) {
-        try (BufferedReader br = Files.newBufferedReader(file)) {
-            String line;
-
-            while ((line = br.readLine()) != null) {
-                System.out.println(line);
-            }
-
-        } catch (IOException e) {
-            throw new RuntimeException();
-        }
-    }
+			while ((line = bufferedReader.readLine()) != null) {
+				System.out.println(line);
+			}
+		} catch (IOException e) {
+			throw new IllegalStateException("A problem occured while reading from a file", e);
+		}
+	}
 
 }
