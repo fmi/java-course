e99e855900d8977737bfa36cdab3c7402b0d6e73
Ivan Angelov <inangelov@abv.bg>
2020-12-13 19:35:41 +0200

    Update DataStream.java

--- 07-io-streams-and-files/snippets/src/bg/sofia/uni/fmi/mjt/io/DataStream.java
+++ 07-io-streams-and-files/snippets/src/bg/sofia/uni/fmi/mjt/io/DataStream.java
@@ -8,38 +8,38 @@ import java.io.IOException;
 
 public class DataStream {
 
-	public static void main(String... args) {
-		writeWithDataStream();
-		readDataStream();
-	}
+    public static void main(String... args) {
+        writeWithDataStream();
+        readDataStream();
+    }
 
-	/**
-	 * Writes structured data using DataOutputStream
-	 * https://docs.oracle.com/javase/8/docs/api/java/io/DataOutputStream.html
-	 */
-	private static void writeWithDataStream() {
-		try (var dataOutputStream = new DataOutputStream(new FileOutputStream("test.dat"))) {
-			dataOutputStream.writeInt(16);
-			dataOutputStream.writeFloat(5.2f);
-			dataOutputStream.writeUTF("potatopotatopotato");
-			dataOutputStream.flush();
-		} catch (IOException e) {
-			throw new IllegalStateException("A problem occured while writing to a file", e);
-		}
-	}
+    /**
+     * Writes structured data using DataOutputStream
+     * https://docs.oracle.com/en/java/javase/15/docs/api/java.base/java/io/DataOutputStream.html
+     */
+    private static void writeWithDataStream() {
+        try (var dataOutputStream = new DataOutputStream(new FileOutputStream("test.dat"))) {
+            dataOutputStream.writeInt(16);
+            dataOutputStream.writeFloat(5.2f);
+            dataOutputStream.writeUTF("potatopotatopotato");
+            dataOutputStream.flush();
+        } catch (IOException e) {
+            throw new IllegalStateException("A problem occurred while writing to a file", e);
+        }
+    }
 
-	/**
-	 * Reads structured data using DataInputStream
-	 * https://docs.oracle.com/en/java/javase/13/docs/api/java.base/java/io/DataInputStream.html
-	 */
-	private static void readDataStream() {
-		try (var dataInputStream = new DataInputStream(new FileInputStream("test.dat"))) {
-			System.out.println("int: " + dataInputStream.readInt());
-			System.out.println("float: " + dataInputStream.readFloat());
-			System.out.println("string: " + dataInputStream.readUTF());
-		} catch (IOException e) {
-			throw new IllegalStateException("A problem occured while reading from a file", e);
-		}
-	}
+    /**
+     * Reads structured data using DataInputStream
+     * https://docs.oracle.com/en/java/javase/15/docs/api/java.base/java/io/DataInputStream.html
+     */
+    private static void readDataStream() {
+        try (var dataInputStream = new DataInputStream(new FileInputStream("test.dat"))) {
+            System.out.println("int: " + dataInputStream.readInt());
+            System.out.println("float: " + dataInputStream.readFloat());
+            System.out.println("string: " + dataInputStream.readUTF());
+        } catch (IOException e) {
+            throw new IllegalStateException("A problem occurred while reading from a file", e);
+        }
+    }
 
 }
