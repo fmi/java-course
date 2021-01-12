292fe6f41a6f28e764762c3e5eaf3101fd19413c
Ivan Angelov <inangelov@abv.bg>
2021-01-12 18:42:26 +0200

    Update Restaurant.java

--- 09-threads/lab/solution/src/bg/sofia/uni/fmi/mjt/restaurant/Restaurant.java
+++ 09-threads/lab/solution/src/bg/sofia/uni/fmi/mjt/restaurant/Restaurant.java
@@ -2,29 +2,30 @@ package bg.sofia.uni.fmi.mjt.restaurant;
 
 public interface Restaurant {
 
-	/**
-	 * Adds an order to the restaurant's backlog.
-	 **/
-	public void submitOrder(Order order);
+    /**
+     * Adds an order to the restaurant's backlog.
+     **/
+    public void submitOrder(Order order);
 
-	/**
-	 * Returns the next order from the restaurant's backlog that has to be cooked.
-	 **/
-	public Order nextOrder();
+    /**
+     * Returns the next order from the restaurant's backlog that has to be cooked.
+     **/
+    public Order nextOrder();
 
-	/**
-	 * Returns the total number of orders submitted to the restaurant's.
-	 **/
-	public int getOrdersCount();
+    /**
+     * Returns the total number of orders submitted to the restaurant's.
+     **/
+    public int getOrdersCount();
 
-	/**
-	 * Returns the restaurant's chefs.
-	 **/
-	public Chef[] getChefs();
+    /**
+     * Returns the restaurant's chefs.
+     **/
+    public Chef[] getChefs();
 
-	/**
-	 * When this method is called, the restaurant is about to close soon and should not serve more orders.
-	 **/
-	public void close();
+    /**
+     * When this method is called, the restaurant is about to close soon and should
+     * not serve more orders.
+     **/
+    public void close();
 
 }
