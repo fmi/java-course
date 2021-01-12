4bd2c2b2e53083371ceeabd9159205d36b745233
Ivan Angelov <inangelov@abv.bg>
2021-01-12 18:41:15 +0200

    Update Order.java

--- 09-threads/lab/solution/src/bg/sofia/uni/fmi/mjt/restaurant/Order.java
+++ 09-threads/lab/solution/src/bg/sofia/uni/fmi/mjt/restaurant/Order.java
@@ -4,20 +4,20 @@ import bg.sofia.uni.fmi.mjt.restaurant.customer.AbstractCustomer;
 
 public record Order(Meal meal, AbstractCustomer customer) implements Comparable<Order> {
 
-	@Override
-	public int compareTo(Order o) {
-		boolean currHasVipCard = this.customer.hasVipCard();
-		boolean oHasVipCard = o.customer.hasVipCard();
+    @Override
+    public int compareTo(Order o) {
+        boolean currHasVipCard = this.customer.hasVipCard();
+        boolean oHasVipCard = o.customer.hasVipCard();
 
-		if (currHasVipCard == oHasVipCard) {
-			return Integer.compare(this.meal.getCookingTime(), o.meal.getCookingTime());
-		}
+        if (currHasVipCard == oHasVipCard) {
+            return Integer.compare(this.meal.getCookingTime(), o.meal.getCookingTime());
+        }
 
-		if (currHasVipCard) {
-			return 1;
-		}
+        if (currHasVipCard) {
+            return 1;
+        }
 
-		return -1;
-	}
+        return -1;
+    }
 
 }
