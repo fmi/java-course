66fa12ff17b69b563c8f450bc17766d7f2663c2e
100yo <vellev@gmail.com>
2023-01-19 12:29:40 +0200

    Enhance Gson snippets with records

---
+++ 12-http-rest/snippets/DevManager.java
@@ -0,0 +1,5 @@
+import com.google.gson.annotations.SerializedName;
+
+public record DevManager(String name, @SerializedName("unit") String department, int level) {
+    // Gson's @SerializedName annotation is also supported for record components
+}
