a67b22462e03181cba860b40bc3acdf91d05676c
Desislava Asenova <desislava.asenova@sap.com>
2023-10-27 19:46:43 +0300

    Fix initiallizers

--- 03-oop-in-java-ii/snippets/src/initializers/InitializersDemo.java
+++ 03-oop-in-java-ii/snippets/src/initializers/InitializersDemo.java
@@ -1,18 +1,17 @@
 package initializers;
 
 class InitializersDemoBase {
-    private static int y;
-
-    static {
-        InitializersDemoBase.y = 1;
-        System.out.println("parent static initializer executed");
-    }
-
     private int x;
+    private static int y;
 
     { // cannot be static - results in compilation error
         this.x = 1;
         System.out.println("Parent initializer executed");
+    }
+
+    static {
+        InitializersDemoBase.y = 1;
+        System.out.println("parent static initializer executed");
     }
 
     public InitializersDemoBase() {
@@ -21,29 +20,28 @@ class InitializersDemoBase {
 }
 
 public class InitializersDemo extends InitializersDemoBase {
-    private static int b;
-
-    static {
-        b = 7;
-        System.out.println("Child static initializer executed");
-    }
-
     private int a;
+    private static int b;
 
     {
         a = 5;
         System.out.println("Child initializer 1 executed");
     }
 
-    {
-        a = 16;
-        System.out.println("Child initializer 2 executed");
+    static {
+        b = 7;
+        System.out.println("Child static initializer executed");
     }
 
     public InitializersDemo() {
         a = 10;
         b = 10;
         System.out.println("Child constructor executed");
+    }
+
+    {
+        a = 16;
+        System.out.println("Child initializer 2 executed");
     }
 
     public static void main(String[] args) {
