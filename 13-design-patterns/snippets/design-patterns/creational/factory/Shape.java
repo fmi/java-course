ffe6556b5850578cd4b78d95e1381069056aa973
100yo <vellev@gmail.com>
2024-01-15 14:58:51 +0200

    Enhance Factory design pattern snippet to include null as switch expression

--- 13-design-patterns/snippets/design-patterns/creational/factory/Shape.java
+++ 13-design-patterns/snippets/design-patterns/creational/factory/Shape.java
@@ -10,7 +10,9 @@ public interface Shape {
             case RECTANGLE -> new Rectangle();
             case SQUARE -> new Square();
             case CIRCLE -> new Circle();
-            default -> null;
+            case null -> null; // Since Java 21, case null can be provided and the selector expression can be null.
+                               // Before Java 21, or if a null case is not present, even if a default case is,
+                               // Shape.of(null) would throw a NPE.
         };
     }
 
