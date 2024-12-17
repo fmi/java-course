61ce89b7c66b20cda316f71028e99c2f3fbdfaeb
Dimitar Tsonev <56803540+DimitarDTsonev@users.noreply.github.com>
2024-12-17 17:25:40 +0200

    Update TFIDFSimilarityCalculator.java

--- homeworks/02-goodreads/resources/src/bg/sofia/uni/fmi/mjt/goodreads/recommender/similaritycalculator/descriptions/TFIDFSimilarityCalculator.java
+++ homeworks/02-goodreads/resources/src/bg/sofia/uni/fmi/mjt/goodreads/recommender/similaritycalculator/descriptions/TFIDFSimilarityCalculator.java
@@ -9,7 +9,6 @@ import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
-
 public class TFIDFSimilarityCalculator implements SimilarityCalculator {
 
     public TFIDFSimilarityCalculator(Set<Book> books, TextTokenizer tokenizer) {}
