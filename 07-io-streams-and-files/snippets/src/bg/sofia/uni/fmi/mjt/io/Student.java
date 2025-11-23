264a68d38a6b75c23f8e072719265bebe0429411
100yo <vellev@gmail.com>
2025-11-23 22:41:41 +0200

    Fix serialization code snippet

--- 07-io-streams-and-files/snippets/src/bg/sofia/uni/fmi/mjt/io/Student.java
+++ 07-io-streams-and-files/snippets/src/bg/sofia/uni/fmi/mjt/io/Student.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 
 public record Student(String name, int age) implements Serializable {
 
+    @Serial
     private static final long serialVersionUID = -4620197539457134073L;
     
 }
